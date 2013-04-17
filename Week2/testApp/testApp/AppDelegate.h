a1f1157d3e17dbaf103de4e89e7a50f1a1ba0584
Edward M murray Jr <ampedej@fullsail.edu>
2013-04-16 22:11:41 -0400

    New single view project added.

---
+++ Week2/testApp/testApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  testApp
+//
+//  Created by Edward Murray on 4/16/13.
+//  Copyright (c) 2013 Edward Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Week2/testApp/testApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Edward Murray on 4/16/13.
+//  Copyright (c) 2013 Edward Murray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
