51294067bd091449cd089b5875825ccf232443d1
Edward M murray Jr <ampedej@fullsail.edu>
2013-04-17 18:49:51 -0400

    Added title and author labels.

--- Week2/testApp/testApp/ViewController.m
+++ Week2/testApp/testApp/ViewController.m
@@ -17,6 +17,24 @@
 - (void)viewDidLoad
 {
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 5.0f, 310.0f, 20.0f)];
+    if (title != nil)
+    {
+        title.backgroundColor = [UIColor greenColor];
+        title.text = @"Title";
+        title.textAlignment = NSTextAlignmentCenter;
+    }
+    UILabel *author = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 25.0f, 70.0f, 20.0f)];
+    if (author != nil)
+    {
+        author.backgroundColor = [UIColor redColor];
+        author.text = @"Author: ";
+        author.textAlignment = NSTextAlignmentRight;
+        author.textColor = [UIColor whiteColor];
+    }
+    [self.view addSubview: author];
+    [self.view addSubview: title];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
