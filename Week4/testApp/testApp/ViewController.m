19284f61e11322013f5af0b0afae66f473909826
Edward M murray Jr <ampedej@fullsail.edu>
2013-05-01 14:45:48 -0400

    Created UILabel for username and UITextField for input.

--- Week4/testApp/testApp/ViewController.m
+++ Week4/testApp/testApp/ViewController.m
@@ -16,6 +16,24 @@
 
 - (void)viewDidLoad
 {
+    self.view.backgroundColor = [UIColor colorWithRed:0.941 green:0.945 blue:0.945 alpha:1];
+    
+    //Label for username
+    UILabel *username = [[UILabel alloc] initWithFrame:CGRectMake(5.0f, 10.0f, 100.0f, 25.0f)];
+    if (username != nil)
+    {
+        username.backgroundColor = [UIColor colorWithRed:0.941 green:0.945 blue:0.945 alpha:1];
+        username.text = @"Username:";
+        username.textAlignment = NSTextAlignmentLeft;
+    }
+    
+    UITextField *userInput = [[UITextField alloc] initWithFrame:CGRectMake(105.0f, 10.0f, 200.0f, 30.0f)];
+    {
+        userInput.borderStyle = UITextBorderStyleRoundedRect;
+    }
+    
+    [self.view addSubview: username];
+    [self.view addSubview: userInput];
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
